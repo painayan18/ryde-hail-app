7a05de0869b35dec684be5f89343109d3cab7e03
painayan18 <nayanspai@gmail.com>
2024-09-17 00:07:31 -0700

    auth screen complete. email auth WIP.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,54 @@
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import {useFonts} from "expo-font";
+import {Stack} from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import {useEffect} from "react";
 import "react-native-reanimated";
+
+import {ClerkProvider, ClerkLoaded} from '@clerk/clerk-expo'
+import {Slot} from 'expo-router'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
-    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
-  });
+    const [loaded] = useFonts({
+        "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+        "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+        "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+        "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (!publishableKey) {
+        throw new Error(
+            'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+        )
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
 
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
+    if (!loaded) {
+        return null;
+    }
+
+    return (
+        <Stack>
+            <ClerkProvider publishableKey={publishableKey}>
+                <ClerkLoaded>
+                    <Stack.Screen name="index" options={{headerShown: false}}/>
+                    <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+                    <Stack.Screen name="(root)" options={{headerShown: false}}/>
+                    <Stack.Screen name="+not-found"/>
+                </ClerkLoaded>
+            </ClerkProvider>
+        </Stack>
+    );
 }
