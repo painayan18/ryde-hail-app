994ff6844fce1b4f3c4e723adad0d42b92c0b698
painayan18 <nayanspai@gmail.com>
2024-09-25 16:37:59 -0700

    Sign up complete

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,68 +1,66 @@
-import {ScrollView, Image, View, Text} from "react-native";
-import {icons, images} from "@/constants";
+import { ScrollView, Image, View, Text } from "react-native";
+import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
-import {useState} from "react";
+import { useState } from "react";
 import CustomButton from "@/components/CustomButton";
-import {Link} from "expo-router";
+import { Link } from "expo-router";
 import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [form, setForm] = useState({
-        email: '',
-        password: '',
-    });
+  const onSignInPress = async () => {};
 
-    const onSignInPress = async () => {
-    }
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome back! 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
 
-    return (
-        <ScrollView className="flex-1 bg-white">
-            <View className="flex-1 bg-white">
-                <View className="relative w-full h-[250px]">
-                    <Image
-                        source={images.signUpCar} className="z-0 w-full h-[250px]"
-                    />
-                    <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-                        Welcome back! 👋
-                    </Text>
-                </View>
-                <View className="p-5">
-                    <InputField
-                        label="Email"
-                        placeholder="Enter your email"
-                        icon={icons.email}
-                        value={form.email}
-                        onChangeText={(value) => setForm({...form, email: value})}
-                    />
-                    <InputField
-                        label="Password"
-                        placeholder="Enter your password"
-                        icon={icons.lock}
-                        secureTextEntry={true}
-                        value={form.password}
-                        onChangeText={(value) => setForm({...form, password: value})}
-                    />
+          <CustomButton
+            title="Log In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
 
-                    <CustomButton
-                        title="Log In"
-                        onPress={onSignInPress}
-                        className="mt-6"
-                    />
+          <OAuth />
 
-                    <OAuth/>
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Don't have an account yet? </Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
 
-                    <Link href="/sign-up"
-                          className="text-lg text-center text-general-200 mt-10">
-                        <Text>Don't have an account yet? </Text>
-                        <Text className="text-primary-500">Sign Up</Text>
-                    </Link>
-                </View>
-
-                {/*    Verification Modal */}
-            </View>
-        </ScrollView>
-    );
+        {/*    Verification Modal */}
+      </View>
+    </ScrollView>
+  );
 };
 
 export default SignIn;
